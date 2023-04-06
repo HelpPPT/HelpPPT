eb4850848927056301643548e70ac3473db8067e
DongHyeon Kim <hyeoni369@naver.com>
2023-04-06 19:35:05 +0900

    파일 추가;

---
+++ src/taskpane/components/WordIntegration.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text } from "@fluentui/react";
+
+export const WordIntegration: React.FunctionComponent = () => {
+  return (
+    <div>
+      <Text>반가워요!</Text>
+    </div>
+  );
+};
