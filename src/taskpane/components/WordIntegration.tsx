e07c745d5d2c42673422dc4e9d3ea43763d42f12
DongHyeon Kim <hyeoni369@naver.com>
2023-04-06 22:05:21 +0900

    일단 뭉치로 가져오는건 미룸

--- src/taskpane/components/WordIntegration.tsx
+++ src/taskpane/components/WordIntegration.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import { PrimaryButton } from "@fluentui/react";
-import axios from "axios";
 
 export const WordIntegration: React.FunctionComponent = () => {
-  const [text, setText] = React.useState<string>("");
-
   const getTextsFromSlides = async (): Promise<Array<string>> =>
     await PowerPoint.run(async (context: PowerPoint.RequestContext) => {
       const textBuffer: Array<string> = [];
@@ -41,20 +38,37 @@ export const WordIntegration: React.FunctionComponent = () => {
     });
 
   // const clusterWords = async (text: string): Promise<Array<Array<string>>> => {
-  const clusterWords = async (text: string) => {
-    const { data } = await axios({
-      method: "POST",
-      url: "http://15.165.217.213:8000/grouping/",
-      data: {
-        sentence: text,
-      },
-    });
-    setText(JSON.stringify(data));
+  const clusterWords = async (__text: string) => {
+    // const { data } = await axios({
+    //   method: "POST",
+    //   url: "http://15.165.217.213:8000/grouping/",
+    //   data: {
+    //     sentence: text,
+    //   },
+    // });
+    const data = [
+      ["자료구조", "스택", "관계"],
+      ["Tree", "표현", "노드", "구조"],
+      ["트리(Tree)", "비선형"],
+      ["OS", "김동현", "계층적", "스터디"],
+      ["스터디", "계층적"],
+      ["20194147", "선형"],
+      ["김동현", "계층적", "스터디"],
+      ["개념", "노드", "구조"],
+      ["노드", "구조"],
+      ["스택", "관계"],
+      ["선형", "20194147"],
+      ["구조", "노드"],
+      ["비선형", "트리(Tree)"],
+      ["계층적", "스터디"],
+      ["관계", "스택"],
+      ["표현", "노드", "구조"],
+    ];
+    console.log(data);
   };
 
   return (
     <div>
-      <textarea value={text} />
       <PrimaryButton
         text="Get Texts From Slides"
         onClick={async () => {
