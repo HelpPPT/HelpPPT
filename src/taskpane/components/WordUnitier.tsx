af0deeb95e333fab743bd9bba1c9dccb2f0957e4
DongHyeon Kim <hyeoni369@naver.com>
2023-04-07 10:23:37 +0900

    url 복구

--- src/taskpane/components/WordUnitier.tsx
+++ src/taskpane/components/WordUnitier.tsx
@@ -41,7 +41,7 @@ export const WordUnitier: React.FunctionComponent = () => {
   const getWordClusters = async (sentence: string): Promise<Array<Array<string>>> => {
     const { data } = await axios({
       method: "POST",
-      url: "https://8v8pkkotrh.execute-api.ap-northeast-2.amazonaws.com/grouping/",
+      url: "https://8v8pkkotrh.execute-api.ap-northeast-2.amazonaws.com/grouping",
       data: { sentence },
     });
 
