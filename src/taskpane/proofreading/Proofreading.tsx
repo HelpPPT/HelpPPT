e1f3734c97ee77a58f43bc4752deb7d6385a94f2
DongHyeon Kim <hyeoni369@naver.com>
2023-05-09 23:52:19 +0900

    문장 나눠서 가져오는거 구현 완료

--- src/taskpane/proofreading/Proofreading.tsx
+++ src/taskpane/proofreading/Proofreading.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { Button, ButtonProps } from "@fluentui/react-components";
 import { getTextsFromSlides, Text } from "../common";
+import axios from "axios";
 
 const Proofreading: React.FC = (props: ButtonProps) => {
-  const splitSentences = async () => {
-    const texts: Array<Text> = await getTextsFromSlides();
-    console.log(texts);
+  const dododo = async () => {
+    const textDatas: Array<Text> = await getTextsFromSlides();
+    const texts: Array<string> = textDatas.map((textData) => textData.text);
+
+    const sentences = await splitSentences(texts);
+    console.log(sentences);
+  };
+
+  const splitSentences = async (sentences: Array<string>): Promise<Array<string>> => {
+    const { data } = await axios({
+      method: "POST",
+      url: "http://127.0.0.1:8000/sentence-split",
+      data: { sentences },
+    });
+
+    return data.sentences;
   };
 
   return (
-    <Button {...props} appearance="primary" onClick={splitSentences}>
+    <Button {...props} appearance="primary" onClick={dododo}>
       Example
     </Button>
   );
