af96e6898a3526f19e2f1a9d3cfb5574171f104f
nahyun902 <nahyun902@naver.com>
2023-05-23 02:17:28 +0900

    안쓰는 api 함수 삭제

--- src/wordunification/api/powerpoint.ts
+++ src/wordunification/api/powerpoint.ts
@@ -1,38 +1,3 @@
-export const getTextsFromSlides = async (): Promise<Array<string>> =>
-  await PowerPoint.run(async (context: PowerPoint.RequestContext) => {
-    const textBuffer: Array<string> = [];
-
-    const slides = context.presentation.slides;
-
-    context.load(slides, "id,shapes/items/type");
-    await context.sync();
-
-    for (const slide of slides.items) {
-      // console.log("Slide ID:", slide.id);
-
-      for (const shape of slide.shapes.items) {
-        if (shape.type === "Unsupported") {
-          continue;
-        }
-
-        context.load(shape, "textFrame/hasText");
-        await context.sync();
-
-        if (!shape.textFrame.hasText) {
-          continue;
-        }
-
-        context.load(shape, "textFrame/textRange/text");
-        await context.sync();
-
-        textBuffer.push(shape.textFrame.textRange.text.trim().replace(/[\n\r\v]/g, "\n"));
-
-        // console.log("Text:", shape.textFrame.textRange.text.replace(/[\n\r\v]/g, "\n"));
-      }
-    }
-    return textBuffer;
-  });
-
 export const unifyWordAll = async (from: Array<string>, to: string) =>
   await PowerPoint.run(async (context: PowerPoint.RequestContext) => {
     console.log(`${from} -> ${to}`);
