4f11c81aa82c2e35e3f312134b31653efa12f4a9
bgb10 <bgb10@naver.com>
2023-05-30 14:02:01 +0900

    contents 부분의 validator 추가

--- src/proofreading/validator.tsx
+++ src/proofreading/validator.tsx
@@ -55,9 +55,9 @@ export const validateSentence = (slideText: SlideText): SentenceValidationResult
       message: "문장이 마침표, 물음표, 느낌표로 끝나지 않았어요.",
     },
     {
-      validatorFunc: validateCommaSpacing,
+      validatorFunc: validatePunctuationSpacing,
       badgeStyle: mergeClasses(styles.badge, styles.orangeBadge),
-      message: "쉼표 뒤에는 띄어쓰기를 해주세요.",
+      message: "구두점 뒤에는 띄어쓰기를 해주세요.",
     },
     {
       validatorFunc: validateStartWithCapital,
@@ -78,6 +78,31 @@ export const validateSentence = (slideText: SlideText): SentenceValidationResult
       validatorFunc: validateNoDoubleNegatives,
       badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
       message: "'안'이나 '않'이 연속되었어요.",
+    },
+    {
+      validatorFunc: validateClosingBrackets,
+      badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
+      message: "괄호가 감싸지지 않았어요.",
+    },
+    {
+      validatorFunc: validateColonNotSpacing,
+      badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
+      message: ": 앞에 띄어쓰기가 있어요.",
+    },
+    {
+      validatorFunc: validateMissingQuotationMarksBeforeRago,
+      badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
+      message: '라고 앞에 " 가 없어요.',
+    },
+    {
+      validatorFunc: validateMissingClosedQuotationMarks,
+      badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
+      message: '" 로 완전히 둘러쌓이지 않았어요.',
+    },
+    {
+      validatorFunc: validateFirstCharacterCapitalLetter,
+      badgeStyle: mergeClasses(styles.badge, styles.blueBadge),
+      message: "문장의 처음은 대문자로 시작해야 해요.",
     },
   ];
 
@@ -104,8 +129,10 @@ const validateEndWithPeriodOrQuestionOrExclamation = (input: string): boolean =>
   return /.*[.|?|!]$/.test(input);
 };
 
-const validateCommaSpacing = (input: string): boolean => {
-  return !/,[^ ]/.test(input);
+const validatePunctuationSpacing = (input: string): boolean => {
+  if (!/\s*[,;?!]\s*/.test(input)) return true;
+  else if (input.lastIndexOf(",") === input.length - 1) return true;
+  else return /[,;?!]\s+/.test(input);
 };
 
 const validateStartWithCapital = (input: string): boolean => {
@@ -116,10 +143,46 @@ const validateNoConsecutiveSpaces = (input: string): boolean => {
   return !/\s{2,}/.test(input);
 };
 
+const validateClosingBrackets = (input: string): boolean => {
+  let open = 0,
+    closed = 0;
+  for (let c of input) {
+    if (c == "(") open++;
+    else if (c == ")") closed++;
+  }
+
+  return open === closed;
+};
+
+const validateMissingQuotationMarksBeforeRago = (input: string): boolean => {
+  // 라고 앞에 처음 나오는 단어(space) 제외하고 " 가 있어야 한다.
+  if (!/(라고)/.test(input)) return true;
+  else return /["”]\s*라고/.test(input);
+};
+
+const validateMissingClosedQuotationMarks = (input: string): boolean => {
+  let open = 0,
+    closed = 0;
+  for (let c of input) {
+    if (c == '"' || c == "“") open++;
+    else if (c == '"' || c == "”") closed++;
+  }
+
+  return open === closed;
+};
+
+const validateColonNotSpacing = (input: string): boolean => {
+  return !/\s:/.test(input);
+};
+
 const validateSingleQuestionOrExclamation = (input: string): boolean => {
   return (input.match(/\?|!/g) || []).length <= 1;
 };
 
 const validateNoDoubleNegatives = (input: string): boolean => {
   return !/안\s*[^ ]*\s*않았다/.test(input);
+};
+
+const validateFirstCharacterCapitalLetter = (input: string): boolean => {
+  return !/^[a-z]/.test(input);
 };
