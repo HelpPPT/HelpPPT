a74ba6760da6d75c9156a61b5ec3c51894f9b79e
DongHyeon Kim <hyeoni369@naver.com>
2023-06-05 13:44:26 +0900

    밑에 카드가 너무 붙어서 디바이더 하나 넣어서 띄움

--- src/proofreading/Proofreading.tsx
+++ src/proofreading/Proofreading.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { getSentencesFromSlides, groupSlideTextsBySlide } from "../common";
 import { SlideTexts } from "../common/main";
-import { makeStyles, Spinner } from "@fluentui/react-components";
+import { Divider, makeStyles, Spinner } from "@fluentui/react-components";
 import { SlideValidation } from "./SlideValidation";
 
 const useStyles = makeStyles({
@@ -29,6 +29,9 @@ export const Proofreading: React.FC = () => {
   return loading ? (
     <Spinner className={styles.loader} label="문장 불러오는중..." labelPosition="below" size="huge" />
   ) : (
-    <div>{...slidesValidations}</div>
+    <div>
+      {...slidesValidations}
+      <Divider />
+    </div>
   );
 };
