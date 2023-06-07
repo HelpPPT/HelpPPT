8e450600a4a6099ce49214ea29647984a6c26418
DongHyeon Kim <hyeoni369@naver.com>
2023-06-08 01:59:40 +0900

    핸들러 사용중...

--- src/proofreading/InvalidMessage.tsx
+++ src/proofreading/InvalidMessage.tsx
@@ -35,13 +35,20 @@ export const InvalidMessage: React.FC<InvalidMessageProps> = ({
 
   const setFontSize24 = () => setTimeout(() => setFontSize(slideText, 24), 250);
 
+  const onClickHandler = (() => {
+    if (message === "폰트 사이즈는 24pt 이상이어야 해요.") {
+      return setFontSize24;
+    }
+    return () => {};
+  })();
+
   return (
     <div className={styles.container}>
       <Badge className={badgeStyle} size="tiny" />
       <Body1 className={styles.text}>{message}</Body1>
       <Button
         className={styles.editButton}
-        onClick={setFontSize24}
+        onClick={onClickHandler}
         appearance="subtle"
         size="small"
         icon={<Edit16Filled />}
