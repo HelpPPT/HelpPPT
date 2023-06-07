742014fdbc104c2272d4b92a83401c73927d2026
DongHyeon Kim <hyeoni369@naver.com>
2023-06-08 01:39:02 +0900

    일단 버튼 넣음

--- src/proofreading/InvalidMessage.tsx
+++ src/proofreading/InvalidMessage.tsx
@@ -1,4 +1,5 @@
-import { Badge, Body1, makeStyles, shorthands } from "@fluentui/react-components";
+import { Badge, Body1, Button, makeStyles, shorthands, tokens } from "@fluentui/react-components";
+import { Edit16Filled } from "@fluentui/react-icons";
 import React from "react";
 
 const useStyles = makeStyles({
@@ -9,6 +10,11 @@ const useStyles = makeStyles({
 
   text: {
     ...shorthands.margin(0),
+  },
+
+  editButton: {
+    color: tokens.colorCompoundBrandForeground1,
+    ...shorthands.margin(0, 0, 0, "3px"),
   },
 });
 
@@ -24,6 +30,7 @@ export const InvalidMessage: React.FC<InvalidMessageProps> = ({ badgeStyle, mess
     <div className={styles.container}>
       <Badge className={badgeStyle} size="tiny" />
       <Body1 className={styles.text}>{message}</Body1>
+      <Button className={styles.editButton} appearance="subtle" size="small" icon={<Edit16Filled />} />
     </div>
   );
 };
